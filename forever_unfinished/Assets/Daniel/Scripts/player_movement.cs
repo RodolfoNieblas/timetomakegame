a89e74f5fee45c4a9297b5fe04f125c4d51456f8
Daniel Sandoval <dansan3420@gmail.com>
2020-07-03 22:01:31 -0700

    soda time
    added soda sprite and pink background to the daniel scene UwU

---
+++ forever_unfinished/Assets/Daniel/Scripts/player_movement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+
+    // FixedUpdate is called just before performing any physics calculations
+    void FixedUpdate()
+    {
+        Input
+    }
+}
