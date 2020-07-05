dcb614b13703cabd4735802268b7c54f1d0eeefb
RodolfoNieblas <37163935+RodolfoNieblas@users.noreply.github.com>
2020-07-05 14:56:33 -0700

    The slime can move up
    as I have added the first animation

--- forever_unfinished/Assets/Dolfo/My_Scripts/Player.cs
+++ forever_unfinished/Assets/Dolfo/My_Scripts/Player.cs
@@ -7,18 +7,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public Animator Player_Animator;
+    bool right = false;
+    bool left = false;
+    bool up = false;
+    bool down = false;
 
     void Start()
     {
-        
+        Player_Animator = GetComponent<Animator>();
     }
 
     
     void Update()
     {
+        //Code for Movement
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0f);
         transform.position = transform.position + (movement.normalized * speed * Time.deltaTime);
+
+        //Code for Animator
+        if(Input.GetKeyDown(KeyCode.W)){
+            up = true;
+            Player_Animator.SetBool("Up", up);
+        }
+        else if(Input.GetKeyUp(KeyCode.W)){
+            up = false;
+            Player_Animator.SetBool("Up", up);
+        }
     }
 }
