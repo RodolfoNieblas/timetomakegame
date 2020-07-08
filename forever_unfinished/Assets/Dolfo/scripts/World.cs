03f20bd671c8e18af24d5d3e77ae40a799389cba
RodolfoNieblas <37163935+RodolfoNieblas@users.noreply.github.com>
2020-07-08 06:57:10 -0700

    My game kinda is a game now
    added lots of stuff

--- forever_unfinished/Assets/Dolfo/scripts/World.cs
+++ forever_unfinished/Assets/Dolfo/scripts/World.cs
@@ -11,21 +11,14 @@ public class World : SceneController   //This script is given to GameObject "Gam
 
 
         //if you don't change camera position, the camera will move to player which may look weird or it may be what you want
-        if(prevScene == "Moon" && prevKey == 3){
-            player.position = new Vector3(-5.44f, 0.8324f, 0f);
-            Camera.main.transform.position = new Vector3(-5.44f, 0.8324f, -10f);
+        if(prevScene == "Earth" && prevKey == 1){
+            player.position = new Vector3(-0.21f, -0.5f, 0f);
+            Camera.main.transform.position = new Vector3(-0.21f, -0.5f, -10f);
         }
-        else if(prevScene == "Moon" && prevKey == 4){
-            player.position = new Vector3(5.0761f, -0.492f, 0f);
-            Camera.main.transform.position = new Vector3(5.0761f, -0.492f, -10f);
+        else if(prevScene == "HoleOne" && prevKey == 1){
+            player.position = new Vector3(-2.18f, 2.07f, 0f);
+            Camera.main.transform.position = new Vector3(-2.18f, 2.07f, -10f);
         }
-        else if(prevScene == "Spaceship Store" && prevKey == 1){
-            player.position = new Vector3(0.31f, 8.06f, 0f);
-            Camera.main.transform.position = new Vector3(0.31f, 8.06f, -10f);
-        }
-        else if(prevScene == "Spaceship Store" && prevKey == 2){
-            player.position = new Vector3(0f, -11f, 0f);
-            Camera.main.transform.position = new Vector3(0f, -11f, -10f);
-        }
+       
     }
 }
