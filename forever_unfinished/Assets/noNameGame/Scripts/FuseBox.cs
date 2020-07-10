1ee7af2d116bba53c4172a81d30b57609c7ec3b8
RodolfoNieblas <37163935+RodolfoNieblas@users.noreply.github.com>
2020-07-09 18:34:45 -0700

    yeetus

---
+++ forever_unfinished/Assets/noNameGame/Scripts/FuseBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseBox : MonoBehaviour
+{
+    public light2D myLight;
+
+    void Start()
+    {
+        //light2d.enabled = false;
+    }
+
+    
+   
+}
