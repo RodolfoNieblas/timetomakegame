d6b53447ba9ee5820e3d33ef28871bced0c24e04
RodolfoNieblas <37163935+RodolfoNieblas@users.noreply.github.com>
2020-07-11 15:03:19 -0700

    fuse box added

--- forever_unfinished/Assets/noNameGame/Scripts/FuseBox.cs
+++ forever_unfinished/Assets/noNameGame/Scripts/FuseBox.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal; //for 2D lights
 
 public class FuseBox : MonoBehaviour
 {
-    
+    private Light2D post;
 
     void Start()
     {
-        //light2d.enabled = false;
+        post = GameObject.FindGameObjectWithTag("LightPost").GetComponent<Light2D>();
+        post.enabled = false;
     }
 
-    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")){
+            post.enabled = true;
+        }
+    }
    
 }
