fa9480c3d70e42ad2041ff85a8cb18d9dca98bb8
Daniel Sandoval <dansan3420@gmail.com>
2020-07-11 17:39:57 -0700

    made basic health bar

---
+++ forever_unfinished/Assets/Daniel/Scripts/UI_manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_manager : MonoBehaviour
+{
+    public Slider health_bar;
+    public player_health player_health; // refers to the script player_health.cs
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health_bar.maxValue = player_health.max_health;
+        health_bar.value = player_health.cur_health;
+    }
+}
---
+++ forever_unfinished/Assets/Daniel/Scripts/damage_player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Note: enemy with this script must have "is trigger" checked for this
+// script to work properly
+public class damage_player : MonoBehaviour
+{
+    public int dmg_to_give;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")) {
+            // if the enemy which script is on collides with object which 
+            // has the tag Player than we use the HurtPlayer function in
+            // player_health.cs to reduce the current health.
+            other.gameObject.GetComponent<player_health>().HurtPlayer(dmg_to_give);
+        }
+    }
+}
